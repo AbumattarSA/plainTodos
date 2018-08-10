b983628005859ee83164f4d44df9c03e0a6cc58e
Shadi Abumattar <shadiabumattar@live.com>
2018-08-10 18:42:11 +0100

    Script v8 deprecated, v9 shows todo list in DOM only

--- Script versions/v8.js
+++ Script versions/v8.js
@@ -1,23 +1,41 @@
-// The main javascript file for the todo list. Older versions are available for viewing in the repository to allow for a clear development in ideas to be seen.
+// Version 8
 
-// Modify the todo list.
 var todoList = {
   todos: [],
+  displayTodos: function() {
+    if (this.todos.length === 0) {
+      console.log('Your todo list is empty.')
+    } else {
+      console.log('My todos:');
+      for (i = 0; i < this.todos.length; i++) {
+
+        if (this.todos[i].completed === true) {
+          console.log('(X)', this.todos[i].todoText);
+        } else {
+          console.log('( )', this.todos[i].todoText);
+        }
+      }
+    }
+  },
   addTodo: function(todoText) {
     this.todos.push({
       todoText: todoText,
       completed: false
     });
+    this.displayTodos();
   },
   changeTodo: function(position, todoText) {
     this.todos[position].todoText = todoText;
+    this.displayTodos();
   },
   deleteTodo: function(position) {
     this.todos.splice(position, 1);
+    this.displayTodos();
   },
   toggleCompleted: function(position) {
     var todo = this.todos[position];
     todo.completed = !todo.completed;
+    this.displayTodos();
   },
   toggleAll: function() {
     var completedTodos = 0;
@@ -41,16 +59,18 @@ var todoList = {
         this.todos[i].completed = true ;
       }
     }
+    this.displayTodos();
   }
 };
 
-// Handle user interactions.
 var handlers = {
+  displayTodos: function () {
+  todoList.displayTodos();
+  },
   addTodo: function () {
     var addTodoInput = document.getElementById('addTodoTextInput');
     todoList.addTodo(addTodoInput.value);
     addTodoInput.value = '';
-    view.displayTodos();
   },
   changeTodo: function () {
     var changeTodoPositionInput = document.getElementById('changeTodoPositionInput');
@@ -58,44 +78,18 @@ var handlers = {
     todoList.changeTodo(changeTodoPositionInput.valueAsNumber, changeTodoTextInput.value);
     changeTodoPositionInput.value = '';
     changeTodoTextInput.value = '';
-    view.displayTodos();
   },
   deleteTodo: function () {
     var deleteTodoPositionInput = document.getElementById('deleteTodoPositionInput');
     todoList.deleteTodo(deleteTodoPositionInput.valueAsNumber);
     deleteTodoPositionInput.value = '';
-    view.displayTodos();
   },
   toggleCompleted: function () {
     var toggleCompletedPositionInput = document.getElementById('toggleCompletedPositionInput');
     todoList.toggleCompleted(toggleCompletedPositionInput.valueAsNumber);
     toggleCompletedPositionInput.value = '';
-    view.displayTodos();
   },
   toggleAll: function () {
     todoList.toggleAll();
-    view.displayTodos();
-  }
-};
-
-// Display the todo list to the user.
-var view = {
-  displayTodos: function() {
-    var todosUl = document.querySelector('ul');   
-    todosUl.innerHTML = '';
-    for(var i = 0; i < todoList.todos.length; i++) {
-      var todoLi = document.createElement('li');
-      var todo = todoList.todos[i];
-      var TodoTextWithCompletion = '';
-      
-      if (todo.completed === true) {
-        todoTextWithCompletion = '(X) ' + todo.todoText;
-      } else {
-        todoTextWithCompletion = '( ) ' + todo.todoText;
-      };
-      
-      todoLi.textContent = todoTextWithCompletion;
-      todosUl.appendChild(todoLi);      
-    }
   }
 };
